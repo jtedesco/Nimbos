4de9b030d3e4c4dd9f1af9197e7fe2707626f112
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-03-12 14:16:22 -0500

    Changed IntrepidRASParserTest to use external log & json files

--- test/parser/IntrepidRASParserTest.py
+++ test/parser/IntrepidRASParserTest.py
@@ -1,3 +1,4 @@
+from json import load
 from src.parser.IntrepidRASParser import IntrepidRASParser
 
 __author__ = 'jon'
@@ -64,7 +65,7 @@ class IntrepidRASParserTest(unittest.TestCase):
           Test that parsing an empty log results in empty summarized and log data
         """
 
-        self.parser.logFile = open('SampleEmptyLog')
+        self.parser.logFile = open('intrepid/log/SampleEmptyLog')
 
         parsedData = self.parser.parse()
         summarizedData = self.parser.summarize()
@@ -78,7 +79,7 @@ class IntrepidRASParserTest(unittest.TestCase):
           Test that parsing an invalid log results in empty summarized and log data
         """
 
-        self.parser.logFile = open('SampleInvalidLog')
+        self.parser.logFile = open('intrepid/log/SampleInvalidLog')
 
         parsedData = self.parser.parse()
         summarizedData = self.parser.summarize()
@@ -93,64 +94,9 @@ class IntrepidRASParserTest(unittest.TestCase):
         """
 
         # Setup
-        expectedSummarizedLog = {
-            'RECID': None,
-            'MSG_ID': ['KERN_0802', 'KERN_0804'],
-            'COMPONENT': ['KERNEL'],
-            'SUBCOMPONENT': ['_bgp_unit_ddr'],
-            'ERRCODE': ['_bgp_err_ddr_single_symbol_error', '_bgp_err_ddr_chipkill_error'],
-            'SEVERITY': ['WARN'],
-            'EVENT_TIME': None,
-            'FLAGS': [],
-            'PROCESSOR': ['0'],
-            'NODE': [],
-            'BLOCK': ['ANL-R46-M0-512', 'ANL-R20-R37-16384'],
-            'LOCATION': ['R21-M0-N10-J05', 'R46-M0-N01-J33'],
-            'SERIALNUMBER': ['44V3572YL12K73050CT', '44V3575YL12M80156ZH'],
-            'ECID': ["x'02405004902DA518080377D308AE'", "x'02407D34C1045713100B674608A2'"],
-            'MESSAGE': [
-                'ECC-correctable single symbol error: DDR Controller 0, failing SDRAM address 0x00466e2a0, BPC pin ER118, transfer 0, bit 157, BPC module pin L04, compute trace MEMORY0DATA157, DRAM chip U01, DRAM pin D9.'
-                ,
-                'ECC-correctable chipkill error: DDR Controller 1, failing SDRAM address 0x03afb4180, chipkill location 0x008, either X8 compute DRAM chip U15 or U34.'
-            ]
-        }
-        expectedParsedLog = [{
-            'RECID': '26123930',
-            'MSG_ID': 'KERN_0802',
-            'COMPONENT': 'KERNEL',
-            'SUBCOMPONENT': '_bgp_unit_ddr',
-            'ERRCODE': '_bgp_err_ddr_single_symbol_error',
-            'SEVERITY': 'WARN',
-            'EVENT_TIME': '2009-01-05-00.02.51.162211',
-            'FLAGS': None,
-            'PROCESSOR': '0',
-            'NODE': None,
-            'BLOCK': 'ANL-R46-M0-512',
-            'LOCATION': 'R46-M0-N01-J33',
-            'SERIALNUMBER': '44V3575YL12M80156ZH',
-            'ECID': "x'02405004902DA518080377D308AE'",
-            'MESSAGE': 'ECC-correctable single symbol error: DDR Controller 0, failing SDRAM ' +\
-                       'address 0x00466e2a0, BPC pin ER118, transfer 0, bit 157, BPC module pin L04,' +\
-                       ' compute trace MEMORY0DATA157, DRAM chip U01, DRAM pin D9.'
-        }, {
-            'RECID': '26123943',
-            'MSG_ID': 'KERN_0804',
-            'COMPONENT': 'KERNEL',
-            'SUBCOMPONENT': '_bgp_unit_ddr',
-            'ERRCODE': '_bgp_err_ddr_chipkill_error',
-            'SEVERITY': 'WARN',
-            'EVENT_TIME': '2009-01-05-00.06.44.106651',
-            'FLAGS': None,
-            'PROCESSOR': '0',
-            'NODE': None,
-            'BLOCK': 'ANL-R20-R37-16384',
-            'LOCATION': 'R21-M0-N10-J05',
-            'SERIALNUMBER': '44V3572YL12K73050CT',
-            'ECID': "x'02407D34C1045713100B674608A2'",
-            'MESSAGE': 'ECC-correctable chipkill error: DDR Controller 1, failing SDRAM address 0x03afb4180, chipkill ' +\
-                       'location 0x008, either X8 compute DRAM chip U15 or U34.'
-        }]
-        self.parser.logFile = open('SampleLog')
+        expectedSummarizedLog = load(open('intrepid/json/ExpectedSummarizedLog.json'))
+        expectedParsedLog = load(open('intrepid/json/ExpectedParsedLog.json'))
+        self.parser.logFile = open('intrepid/log/SampleLog')
         self.maxDiff = 2000
 
         # Test
