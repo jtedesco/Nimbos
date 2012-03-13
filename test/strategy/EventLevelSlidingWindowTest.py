ed6a57ccae1afb655ebb5c570b10e02df1c77b8d
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-03-13 15:04:15 -0500

    Refactored EventLevelSlidingWindowTest tests and added training file content for tests

--- test/strategy/EventLevelSlidingWindowTest.py
+++ test/strategy/EventLevelSlidingWindowTest.py
@@ -13,6 +13,20 @@ class EventLevelSlidingWindowTest(unittest.TestCase):
     def setUp(self):
         self.eventLevelSlidingWindowStrategy = EventLevelSlidingWindow()
 
+        # Parsed training data for five sub-window example
+        self.fiveSubWindowTrainingData = [
+            ((2, 0, 1, 0), (1, 0, 1, 0), (0, 3, 0, 0), (0, 0, 0, 0), True),
+            ((1, 0, 1, 0), (0, 3, 0, 0), (0, 0, 0, 0), (1, 0, 0, 1), False),
+            ((0, 3, 0, 0), (0, 0, 0, 0), (1, 0, 0, 1), (0, 0, 0, 0), False)
+        ]
+
+        # Parsed training data for six sub-window example
+        self.sixSubWindowTrainingData = [
+            ((2, 0, 1, 0), (0, 0, 0, 0), (1, 0, 1, 0), (0, 3, 0, 0), (0, 0, 0, 0), True),
+            ((0, 0, 0, 0), (1, 0, 1, 0), (0, 3, 0, 0), (0, 0, 0, 0), (1, 0, 0, 1), False),
+            ((1, 0, 1, 0), (0, 3, 0, 0), (0, 0, 0, 0), (1, 0, 0, 1), (0, 0, 0, 0), False)
+        ]
+
 
     def testParseTrainignDataEmpty(self):
         """
@@ -58,22 +72,15 @@ class EventLevelSlidingWindowTest(unittest.TestCase):
             self.assertEqual('Error parsing windowed log data, could not find SEVERITY field!', error.message)
 
 
-    def testParseTrainignDataFiveSubWindows(self):
+    def testParseTrainingDataFiveSubWindows(self):
         """
           Tests that the training data for the event level sliding window strategy is generated correctly for 5
             sub-window length windows
         """
 
         # Setup
-        mockWindowedLogData = load(open('eventLevelSlidingWindow/json/SampleFiveWindowedEventLevelLog.json'))
-        expectedTrainingData = [
-
-            # Contains tuples of counts for (INFO,WARN,ERROR,FATAL) events for the first five sub-windows,
-            #  and a True/False label to whether or not it contains a FATAL error in the sixth sub-window
-            ((2, 0, 1, 0), (1, 0, 1, 0), (0, 3, 0, 0), (0, 0, 0, 0), True),
-            ((1, 0, 1, 0), (0, 3, 0, 0), (0, 0, 0, 0), (1, 0, 0, 1), False),
-            ((0, 3, 0, 0), (0, 0, 0, 0), (1, 0, 0, 1), (0, 0, 0, 0), False)
-        ]
+        mockWindowedLogData = load(open('eventLevelSlidingWindow/parsedLog/FiveSubWindows.json'))
+        expectedTrainingData = self.fiveSubWindowTrainingData
 
         # Test
         actualTrainingData = self.eventLevelSlidingWindowStrategy.parseWindowedLogData(mockWindowedLogData)
@@ -82,22 +89,15 @@ class EventLevelSlidingWindowTest(unittest.TestCase):
         self.assertEqual(expectedTrainingData, actualTrainingData)
 
 
-    def testParseTrainignDataSixSubWindows(self):
+    def testParseTrainingDataSixSubWindows(self):
         """
           Tests that the training data for the event level sliding window strategy is generated correctly for 6
             sub-window length windows
         """
 
         # Setup
-        mockWindowedLogData = load(open('eventLevelSlidingWindow/json/SampleSixWindowedEventLevelLog.json'))
-        expectedTrainingData = [
-
-            # Contains tuples of counts for (INFO,WARN,ERROR,FATAL) events for the first five sub-windows,
-            #  and a True/False label to whether or not it contains a FATAL error in the sixth sub-window
-            ((2, 0, 1, 0), (0, 0, 0, 0), (1, 0, 1, 0), (0, 3, 0, 0), (0, 0, 0, 0), True),
-            ((0, 0, 0, 0), (1, 0, 1, 0), (0, 3, 0, 0), (0, 0, 0, 0), (1, 0, 0, 1), False),
-            ((1, 0, 1, 0), (0, 3, 0, 0), (0, 0, 0, 0), (1, 0, 0, 1), (0, 0, 0, 0), False)
-        ]
+        mockWindowedLogData = load(open('eventLevelSlidingWindow/parsedLog/SixSubWindows.json'))
+        expectedTrainingData = self.sixSubWindowTrainingData
 
         # Test
         actualTrainingData = self.eventLevelSlidingWindowStrategy.parseWindowedLogData(mockWindowedLogData)
@@ -125,8 +125,27 @@ class EventLevelSlidingWindowTest(unittest.TestCase):
 
 
     def testBuildTrainingFileContentFiveWindow(self):
-        pass
+
+        # Setup
+        trainingData = self.fiveSubWindowTrainingData
+        expectedTrainingFileContent = open('eventLevelSlidingWindow/trainingFile/FiveSubWindows').read()
+
+        # Test
+        actualTrainingFileContent = self.eventLevelSlidingWindowStrategy.buildTrainingFileContent(trainingData)
+
+        # Verify
+        self.assertEqual(expectedTrainingFileContent, actualTrainingFileContent)
+
 
 
     def testBuildTrainingFileContentSixWindow(self):
-        pass
+
+        # Setup
+        trainingData = self.sixSubWindowTrainingData
+        expectedTrainingFileContent = open('eventLevelSlidingWindow/trainingFile/SixSubWindows').read()
+
+        # Test
+        actualTrainingFileContent = self.eventLevelSlidingWindowStrategy.buildTrainingFileContent(trainingData)
+
+        # Verify
+        self.assertEqual(expectedTrainingFileContent, actualTrainingFileContent)
