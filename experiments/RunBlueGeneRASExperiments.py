e48e10bc918b5d7a0c853cdd545ee4c8848a5f37
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-03-15 21:06:13 -0500

    Removed positive-unlabeled experiments

--- experiments/RunBlueGeneRASExperiments.py
+++ experiments/RunBlueGeneRASExperiments.py
@@ -13,9 +13,7 @@ if __name__ == '__main__':
     # The experiments to run
     experiments = [
         EventLevelStrategy('PosNeg5SubWindows5Hours'),
-        EventLevelStrategy('PosNeut5SubWindows5Hours', negativeLabels=False),
         EventLevelStrategy('PosNeg3SubWindows5Hours', numberOfSubWindows=3),
-        EventLevelStrategy('PosNeut3SubWindows5Hours', negativeLabels=False, numberOfSubWindows=3),
         EventLevelStrategy('PosNeg7SubWindows5Hours', numberOfSubWindows=7),
         EventLevelStrategy('PosNeg5SubWindows3Hours', windowDelta=timedelta(hours=3)),
         EventLevelStrategy('PosNeg3SubWindows3Hours', numberOfSubWindows=3, windowDelta=timedelta(hours=3)),
@@ -24,9 +22,7 @@ if __name__ == '__main__':
         EventLevelStrategy('PosNeg3SubWindows7Hours', windowDelta=timedelta(hours=7), numberOfSubWindows=3),
         EventLevelStrategy('PosNeg7SubWindows7Hours', windowDelta=timedelta(hours=7), numberOfSubWindows=7),
         RandomizedEventLevelStrategy('RandomizedPosNeg5SubWindows5Hours'),
-        RandomizedEventLevelStrategy('RandomizedPosNeut5SubWindows5Hours', negativeLabels=False),
         RandomizedEventLevelStrategy('RandomizedPosNeg3SubWindows5Hours', numberOfSubWindows=3),
-        RandomizedEventLevelStrategy('RandomizedPosNeut3SubWindows5Hours', negativeLabels=False, numberOfSubWindows=3),
         RandomizedEventLevelStrategy('RandomizedPosNeg7SubWindows5Hours', numberOfSubWindows=7),
         RandomizedEventLevelStrategy('RandomizedPosNeg5SubWindows3Hours', windowDelta=timedelta(hours=3)),
         RandomizedEventLevelStrategy('RandomizedPosNeg3SubWindows3Hours', numberOfSubWindows=3, windowDelta=timedelta(hours=3)),
