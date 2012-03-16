2bbbb35301538f3999596f57e18f8018228b4e35
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-03-15 21:02:49 -0500

    Implemented RandomizedEventLevelStrategy

--- src/strategy/slidingWindow/RandomizedEventLevelStrategy.py
+++ src/strategy/slidingWindow/RandomizedEventLevelStrategy.py
@@ -1,3 +1,4 @@
+from random import sample
 from src.strategy.slidingWindow.EventLevelStrategy import EventLevelStrategy
 
 __author__ = 'jon'
@@ -7,3 +8,24 @@ class RandomizedEventLevelStrategy(EventLevelStrategy):
       Randomized adaptation of event level sliding window strategy, where we randomly select some subset of negative
         training examples so that there an equal number of positive and negative training examples.
     """
+
+    def parseWindowedLogData(self, windowedLogData):
+        """
+          Parse the windowed log data, removing random entries so that the counts of positive & negative examples match
+        """
+
+        fullTrainingData = super(RandomizedEventLevelStrategy, self).parseWindowedLogData(windowedLogData)
+
+        positiveTrainingExamples = []
+        negativeTrainingExamples = []
+
+        # Iterate through the training data, separating into positive & negative examples
+        for trainingEntry in fullTrainingData:
+            trainingExampleList = positiveTrainingExamples if trainingEntry[-1] else negativeTrainingExamples
+            trainingExampleList.append(trainingEntry)
+
+        # Pull out the proper number of random examples and combine it back into mixed training examples
+        randomNegativeTrainingExamples = sample(negativeTrainingExamples, len(positiveTrainingExamples))
+        trainingData = positiveTrainingExamples + randomNegativeTrainingExamples
+
+        return trainingData
